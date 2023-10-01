d0778254ce0dc6ec1a23e5eddc7e09cdc216d697
nomayen <nomayen.ohin@gmail.com>
2023-10-02 00:38:56 +0600

    first commit

---
+++ script/deposit.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    
+        const newDepositAmount = getInputFieldValueById('deposit-filed')
+
+        const previousDepositTotal = getTextElementValueById('deposit-total');
+
+        
+        const newDepositTotal = newDepositAmount + previousDepositTotal;
+        
+        setTextElementValueById('deposit-total', newDepositTotal)
+        
+        const previousBalanceTotal = getTextElementValueById('balance-total');
+
+        const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+        setTextElementValueById('balance-total', newBalanceTotal)
+
+    
+
+
+})
